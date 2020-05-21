d846743835e67b1f554108baada668c9ca5b2efe
unknown <0622okakyo@gmail.com>
2020-05-22 04:21:26 +0900

    コントローラー関数追加

---
+++ collectImg/controller/setting.py
@@ -0,0 +1,66 @@
+# TODO: creating the update setting fuction later if needed
+
+
+# import Some Packages
+import yaml
+
+
+def createSetting(*args):
+    """
+    Setting This application Information
+
+    parameters
+    ----------
+    args: Objects
+        arguments when you call this Tools
+
+    """
+
+    try:
+        pass
+    except Error as EOFError:
+        pass
+    finally:
+        pass
+
+    return
+
+
+def readSetting(*args):
+    """
+    Reading the setting information
+
+    parameters
+    ----------
+    args: Objects
+        arguments when you call this Tools
+
+    """
+
+    try:
+        pass
+    except expression as identifier:
+        pass
+    finally:
+        pass
+    return
+
+
+def deleteSetting(*args):
+    """
+    Deleting some selected Information
+
+    parameters
+    ----------
+    args: Objects
+        arguments when you call this Tools
+
+    """
+
+    try:
+        pass
+    except expression as identifier:
+        pass
+    finally:
+        pass
+    return
--- mkdata/useage/search.py
+++
@@ -1,6 +0,0 @@
-import argparse
-
-
-def readAll():
-    return ""
-
